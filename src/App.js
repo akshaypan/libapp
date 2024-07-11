a79b425cb0a6cb741df6c2492d807281adab5008
akshaypan <akshaybenjamin7@gmail.com>
2024-07-11 19:41:55 +0530

    succ

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Home from './Components/Home';
 import Addbook from './Components/Addbook';
 import Viewallbook from './Components/Viewallbook';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-     <Home/>
-     <Addbook/>
-     <Viewallbook/>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/addb' element={<Addbook/>}/>
+      <Route path='/viewb' element={<Viewallbook/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Navigationbar from './Navigationbar'
 
 const Home = () => {
   return (
     
     <div>
       
+      <Navigationbar/>
        <div className="container">
         <div className="row">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
